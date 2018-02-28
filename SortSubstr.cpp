3de264e8155c99b89b263da782b94cb8ea0278ae
Akanksha <36906392+akank20@users.noreply.github.com>
2018-02-28 14:45:26 -0800

    Create SortSubstr.cpp

---
+++ SortSubstr.cpp
@@ -0,0 +1,61 @@
+
+#include <iostream>
+#include<string>
+#include <bits/stdc++.h>
+
+using namespace std;
+void DesSort(char[],int, int);
+
+int main() {
+	int num,N,M;
+	string S;
+	char str[100];
+	cin >> num;										// Reading input from STDIN
+	for(int i=0;i<num;i++)
+	{
+	    cin>>S>>N>>M;
+	    strcpy(str, S.c_str());
+	    DesSort(str,N,M);
+	    cout<<endl;
+	}
+	return 0;
+}
+
+void DesSort(char S[], int N, int M)
+{
+    int i,j,len;
+    char sortS[100];
+    char max,temp;
+    len=sizeof(S)/sizeof(*S);
+    if(N>0)
+    {
+        for(i=0;i<N;i++)
+        {
+            sortS[i]=S[i];
+            cout<<sortS[i];
+        }
+    }
+    for(i=N;i<=M;i++)
+    {   max=S[i];
+        for(j=i+1;j<=M;j++)
+        {
+            if(max<S[j])
+            {
+                temp=max;
+                max=S[j];
+                S[j]=temp;
+            }
+        }
+        sortS[i]=max;
+        cout<<sortS[i];
+        cout<<sortS[i];
+    }
+    if(S[i]!='\0')
+    {
+        for(i;S[i]!='\0';i++)
+        {
+            sortS[i]=S[i];
+            cout<<sortS[i];
+        }
+    }
+}
